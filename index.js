c557301afd2eb3dc3726d904172f52478317e7ac
Leenday <iammilagre@gmail.com>
2021-09-30 17:39:18 +0300

    commit for questions

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 
-const getTemplate = (title, description) => {
-  return `<h1>${title, description}</h1>`
+const getTemplateItem = (title, description) => {
+  return `<p>${title}</p><p>${description}</p>`;
 }
 
+const handleAddButton = (title, description) => {
+  // adding elements values to variables and save state
+  const cardData = {
+    title: title,
+    description: description,
+    done: false
+  }
+  CARDS_STORAGE.push(cardData)
+}
+
+const renderItem = () => {
+  // find element and render item
+  CARDS_STORAGE.map(card => {
+    const div = document.createElement('div')
+    div.textContent = getTemplateItem(card['title'], card['description'])
+    document.body.append(div)
+  })
+}
+
+const CARDS_STORAGE = []
