f823ea9e6fac1739362fdff258f248b51df9e6ea
Leenday <iammilagre@gmail.com>
2021-10-01 15:33:36 +0300

    add localStorage and bit of refactoring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-const CARDS_STORAGE = []
+localStorage.setItem('cards_counter', 0)
+localStorage.setItem('cards_storage', JSON.stringify([]))
 
 const getTemplateItem = (title, description) => {
   return `<p>${title}</p><p>${description}</p>`;
@@ -10,20 +11,33 @@ const handleAddButton = (event) => {
   const title = document.getElementsByName('title')[0].value
   const description = document.getElementsByName('description')[0].value
   const cardData = {
-    id: CARDS_STORAGE.length,
+    id: localStorage.cards_counter,
     title: title,
     description: description,
     done: false
   }
-  CARDS_STORAGE.push(cardData)
-  event.stopPropagation();
+  // need to parse localStorage first
+  let parsedCardsStorage = JSON.parse(localStorage.cards_storage)
+  let parsedCardsCounter = JSON.parse(localStorage.cards_counter)
+  parsedCardsStorage.push(cardData)
+  // then set new values
+  localStorage.setItem('cards_storage', JSON.stringify(parsedCardsStorage))
+  parsedCardsCounter += 1
+  localStorage.setItem('cards_counter', JSON.stringify(parsedCardsCounter))
+  renderItems()
+  event.stopPropagation()
 }
 
-const renderItem = () => {
+const renderItems = () => {
   // find element and render item
-  CARDS_STORAGE.forEach(card => {
-    const div = document.createElement('div')
-    div.textContent = getTemplateItem(card.title, card.description)
-    document.body.append(div)
+  cards = JSON.parse(localStorage.cards_storage)
+  cards.forEach(card => {
+    renderItem(card)
   })
+}
+
+const renderItem = (card) => {
+  const div = document.createElement('div')
+  div.textContent = getTemplateItem(card.title, card.description)
+  document.body.append(div)
 }
