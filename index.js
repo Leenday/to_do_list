8cc18666c49f108523131908d9df3365971a0ab6
Leenday <iammilagre@gmail.com>
2021-10-01 13:45:31 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 
+const CARDS_STORAGE = []
+
 const getTemplateItem = (title, description) => {
   return `<p>${title}</p><p>${description}</p>`;
 }
 
-const handleAddButton = (title, description) => {
+const handleAddButton = (event) => {
   // adding elements values to variables and save state
+  const title = document.getElementsByName('title')[0].value
+  const description = document.getElementsByName('description')[0].value
   const cardData = {
+    id: CARDS_STORAGE.length,
     title: title,
     description: description,
     done: false
   }
   CARDS_STORAGE.push(cardData)
+  event.stopPropagation();
 }
 
 const renderItem = () => {
   // find element and render item
-  CARDS_STORAGE.map(card => {
+  CARDS_STORAGE.forEach(card => {
     const div = document.createElement('div')
-    div.textContent = getTemplateItem(card['title'], card['description'])
+    div.textContent = getTemplateItem(card.title, card.description)
     document.body.append(div)
   })
 }
-
-const CARDS_STORAGE = []
